5f80d8101f3f0c40e30c1700aabb56d4caafc294
Michael <michaeljrdeitz@gmail.com>
2024-07-19 20:40:48 -0500

    added a default image for vehicles if one is not added and finshed the workorder card

--- client/src/components/workorders/WorkOrderList.jsx
+++ client/src/components/workorders/WorkOrderList.jsx
@@ -1,44 +1,57 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Badge, Button, ButtonGroup, Card, CardBody, CardTitle, Input, ListGroup, ListGroupItem } from "reactstrap";
+import { Badge, Button, ButtonGroup, Card, CardBody, CardTitle, ListGroup, ListGroupItem } from "reactstrap";
 import { getWorkOrders } from "../../managers/workOrderManager";
 import PageContainer from "../PageContainer";
+import DefaultImage from "../../resources/DefaultImage.jpg";
 
 export default function WorkOrderList({ loggedInUser }) {
-    const [workOrders, setWorkOrders] = useState([])
+    const [workOrders, setWorkOrders] = useState([]);
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     useEffect(() => {
-        getWorkOrders().then(setWorkOrders)
-    }, [])
+        getWorkOrders().then(setWorkOrders);
+    }, []);
 
-    return( 
+    return (
         <PageContainer>
             <div>
                 <h1>Work Orders</h1>
             </div>
             <div>
                 <ButtonGroup>
-                    <Button color="success">Add WorkOrder</Button>
+                    <Button color="success" onClick={() => navigate("create")}>Add WorkOrder</Button>
                 </ButtonGroup>
             </div>
             <div>
                 {workOrders.map((wo) => (
-                   <Card key={wo.id}>
-                    <CardBody>
-                        <CardTitle className="d-flex justify-content-center">{wo.vehicles.brand.make} {wo.vehicles.size.cubicCentimeters}cc</CardTitle>
-                        <div className="d-flex justify-content-center">{wo.vehicles.typeOfVehicle.type}</div>
-                        <ListGroup className="m-2">
-                            <ListGroupItem>
-                                <Badge>Work To Do :</Badge> {wo.description}
-                            </ListGroupItem>
-                        </ListGroup>
-                        <Button color="primary">View WorkOrder</Button>
-                    </CardBody>
-                   </Card> 
+                    <Card key={wo.id} style={{ width: "20rem"}} className="shadow-sm">
+                        <CardBody>
+                            <CardTitle className="d-flex justify-content-center">
+                                {wo.vehicles.brand.make} {wo.vehicles.size.cubicCentimeters}cc
+                            </CardTitle>
+                            <div className="d-flex justify-content-center">
+                                <img 
+                                    src={wo.vehicles.imageUrl ? wo.vehicles.imageUrl : DefaultImage} 
+                                    alt={wo.vehicles.typeOfVehicle.type}
+                                    style={{ width: '50%', height: 'auto' }} 
+                                    onError={(e) => { e.target.onerror = null; e.target.src = DefaultImage; }}
+                                />
+                            </div>
+                            <ListGroup className="m-2">
+                                <ListGroupItem>
+                                    <Badge>Work To Do :</Badge> {wo.description}
+                                </ListGroupItem>
+                            </ListGroup>
+                            <div className="d-flex justify-content-center">
+                            <Button color="primary" onClick={() => navigate("View")}>View WorkOrder</Button>
+                            </div>
+                        </CardBody>
+                    </Card>
                 ))}
             </div>
         </PageContainer>
-    )
+    );
 }
+
