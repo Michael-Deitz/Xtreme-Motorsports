83035c630c0babc08b89a889c7d034b074d6df49
Michael <michaeljrdeitz@gmail.com>
2024-07-30 12:35:30 -0500

    created the fetch to create a work order and started the page layout for a cretion

--- client/src/components/workorders/WorkOrderCreate.jsx
+++ client/src/components/workorders/WorkOrderCreate.jsx
@@ -1,5 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import { createWorkOrder } from "../../managers/workOrderManager";
+import PageContainer from "../PageContainer";
+import { Button, ButtonToolbar, Card, CardBody, Form, FormGroup, Input, Label } from "reactstrap";
+
 export default function WorkOrderCreate() {
     
+    const navigate = useNavigate();
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const workOrder = {
+            description: description,
+            vehiclesId: vehiclesId
+        };
+
+        createWorkOrder(workOrder).then(() => navigate("/workorders"));
+    }
+
+    return (
+        <PageContainer>
+            <h4>Create A Work Order</h4>
+            <Card>
+                <CardBody>
+                    
+                </CardBody>
+            </Card>
+        </PageContainer>
+    )
 
 } 
--- client/src/managers/workOrderManager.jsx
+++ client/src/managers/workOrderManager.jsx
@@ -3,3 +3,13 @@ const _apiUrl = "/api/workorder"
 export const getWorkOrders = () => {
     return fetch(_apiUrl).then((res) => res.json());
 }
+
+export const createWorkOrder = (workOrder) => {
+    return fetch(`${_apiUrl}/create`, {
+        method: "POST",
+        headers: {
+            'Content-Type' : 'application.json'
+        },
+        body: JSON.stringify(workOrder)
+    });
+}
